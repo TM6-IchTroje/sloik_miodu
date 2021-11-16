8632fcf39b0fe132857da2859022b226823a88a2
NickiMina <olkasia@poczta.one.pl>
2021-11-16 13:28:52 +0100

    Bazowy front end dla ekranu logowania oraz przycisk do rejestracji

--- lib/logInScreen.dart
+++ lib/logInScreen.dart
@@ -1,34 +1,77 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'mainScreen.dart';
+import 'signup.dart';
 
 class logInScreen extends StatelessWidget {
   const logInScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFEF642D)),
-      home: Material(
-        child: Stack(
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Material(
-                  child: Row(
-                    children: <Widget>[
-                      Text("Zarejestruj się!",
-                      style: TextStyle(
-                        color: Colors.white
-                      ),)
-                    ],
+            Container(
+              height: 500,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text("Słoik miodu", style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
                   ),
-                )
-              ],
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Email",
+                      hintStyle: TextStyle(color: Colors.white)),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Hasło:",
+                        suffixIcon: GestureDetector(
+                          child: Icon(Icons.visibility,
+                            color: Colors.white,
+                          ),
+                        ),
+                        hintStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    height: 45,
+                    width: double.infinity,
+                    child: RaisedButton(
+                        child: Text("Zaloguj się"),
+                        color: Colors.white,
+                        onPressed: (){}),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (ctx)=>signup()));
+                        },
+                        child: Text("Zarejestruj się!", style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),),
+                      )
+
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
-
       ),
     );
   }
