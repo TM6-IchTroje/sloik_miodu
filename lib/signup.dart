f7fd18c5612da2e95f2181454df76068660b4e5a
ProjectAntZ <luki111.1998@gmail.com>
2021-11-16 20:07:41 +0100

    Sign up screen.

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:sloik_miodu/logInScreen.dart';
 
 class signup extends StatelessWidget {
   const signup({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.deepOrange,
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 500,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text("Słoik miodu", style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Email",
+                        hintStyle: TextStyle(color: Colors.white)),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Hasło:",
+                      suffixIcon: GestureDetector(
+                        child: Icon(Icons.visibility,
+                          color: Colors.white,
+                        ),
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Potwierdź hasło:",
+                      suffixIcon: GestureDetector(
+                        child: Icon(Icons.visibility,
+                          color: Colors.white,
+                        ),
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    height: 45,
+                    width: double.infinity,
+                    child: RaisedButton(
+                        child: Text("Zarejestruj się"),
+                        color: Colors.white,
+                        onPressed: (){}),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (ctx)=>logInScreen()));
+                        },
+                        child: Text("Masz już konto? Zaloguj się!", style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
