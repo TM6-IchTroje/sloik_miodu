aa634fa68f811cb9c8ac973101e859495ac00ca2
ProjectAntZ <luki111.1998@gmail.com>
2021-11-24 10:14:56 +0100

    Signup verification and error.

--- lib/signup.dart
+++ lib/signup.dart
@@ -5,9 +5,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 class signup extends StatelessWidget {
   signup({Key? key}) : super(key: key);
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   TextEditingController emailController = TextEditingController();
   TextEditingController pass1Controller = TextEditingController();
   TextEditingController pass2Controller = TextEditingController();
+
+  String emailErr = "";
+  String passErr = "";
 
   @override
   Widget build(BuildContext context) {
@@ -21,77 +26,109 @@ class signup extends StatelessWidget {
             Container(
               height: 500,
               width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text("Słoik miodu", style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                  ),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Email",
-                        hintStyle: TextStyle(color: Colors.white)),
-                  ),
-                  TextFormField(
-                    controller: pass1Controller,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Hasło:",
-                      suffixIcon: GestureDetector(
-                        child: Icon(Icons.visibility,
-                          color: Colors.white,
-                        ),
-                      ),
-                      hintStyle: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  TextFormField(
-                    controller: pass2Controller,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Potwierdź hasło:",
-                      suffixIcon: GestureDetector(
-                        child: Icon(Icons.visibility,
-                          color: Colors.white,
-                        ),
-                      ),
-                      hintStyle: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  Container(
-                    height: 45,
-                    width: double.infinity,
-                    child: RaisedButton(
-                        child: Text("Zarejestruj się"),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text("Słoik miodu", style: TextStyle(
+                        fontSize: 40,
                         color: Colors.white,
-                        onPressed: (){
-                          if (pass1Controller.text == pass2Controller.text) {
-                            FirebaseAuth.instance.createUserWithEmailAndPassword(
-                                email: emailController.text,
-                                password: pass1Controller.text
-                            );
-                          }
-                        }),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      GestureDetector(
-                        onTap: (){
-                          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (ctx)=>logInScreen()));
-                        },
-                        child: Text("Masz już konto? Zaloguj się!", style: TextStyle(
+                        fontWeight: FontWeight.bold),
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Email",
+                          hintStyle: TextStyle(color: Colors.white)),
+                      validator: (value) {
+                        if (emailErr.isNotEmpty) {
+                          return emailErr;
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: pass1Controller,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Hasło:",
+                        suffixIcon: GestureDetector(
+                          child: Icon(Icons.visibility,
+                            color: Colors.white,
+                          ),
+                        ),
+                        hintStyle: TextStyle(color: Colors.white),
+                      ),
+                      validator: (value) {
+                        if (passErr.isNotEmpty) {
+                          return passErr;
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: pass2Controller,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Potwierdź hasło:",
+                        suffixIcon: GestureDetector(
+                          child: Icon(Icons.visibility,
+                            color: Colors.white,
+                          ),
+                        ),
+                        hintStyle: TextStyle(color: Colors.white),
+                      ),
+                      validator: (value) {
+                        if (pass1Controller.text != pass2Controller.text) {
+                          return "Password does not match";
+                        }
+                        return null;
+                      },
+                    ),
+                    Container(
+                      height: 45,
+                      width: double.infinity,
+                      child: RaisedButton(
+                          child: Text("Zarejestruj się"),
                           color: Colors.white,
-                          fontSize: 20,
-                        ),),
-                      )
-                    ],
-                  )
-                ],
+                          onPressed: (){
+                          if (_formKey.currentState!.validate()) {
+                            try {
+                              FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                  email: emailController.text,
+                                  password: pass1Controller.text
+                              );
+                            } on FirebaseAuthException catch (e) {
+                              if (e.code == 'weak-password') {
+                                passErr = 'The password provided is too weak.';
+                              } else if (e.code == 'email-already-in-use') {
+                                emailErr = 'The account already exists for that email.';
+                              }
+                            }
+                          }
+
+                            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (ctx)=>logInScreen()));
+                          }),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (ctx)=>logInScreen()));
+                          },
+                          child: Text("Masz już konto? Zaloguj się!", style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
             )
           ],
