9ef151fcc10d57a6f43f28b65c1c4f3d87fd767d
majewskimateusz <kontakt@mateuszmajewski.com>
2021-12-06 20:50:46 +0100

    Dodano funkcjonalne ekrany logowania i rejestracji

--- lib/view/screens/log_in_screen.dart
+++ lib/view/screens/log_in_screen.dart
@@ -1,79 +1,179 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class logInScreen extends StatelessWidget {
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  static String p =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regExp = new RegExp(p);
+
+  void validation() async {
+    final FormState _form = _formKey.currentState;
+    if (_formKey.currentState.validate()) {
+      print("yes");
+    } else {
+      print("no");
+    }
+  }
+
+  Future<bool> checkLogin(TextEditingController emailController,
+      TextEditingController passwordController) async {
+    try {
+      AuthResult userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+    } catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+        return false;
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+        return false;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool obserText = true;
+
   @override
   Widget build(BuildContext context) {
+
+    void changeToMain(bool answer) {
+      if (answer == true) {
+        Navigator.pushNamed(context, '/allProductsScreen');
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Zaloguj się'),
       ),
+      key: _scaffoldKey,
       backgroundColor: Colors.deepOrange,
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: 500,
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    "Słoik miodu",
-                    style: TextStyle(
-                        fontSize: 40,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
+      body: Form(
+        key: _formKey,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 500,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      "Słoik miodu",
+                      style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      validator: (value) {
+                        if (value == "") {
+                          return "Podaj email";
+                        } else if (!regExp.hasMatch(value)) {
+                          return "Email jest nieprawidłowy!";
+                        }
+                        return "";
+                      },
+                      decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: "Email",
-                        hintStyle: TextStyle(color: Colors.white)),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Hasło:",
-                      suffixIcon: GestureDetector(
-                        child: Icon(
-                          Icons.visibility,
-                          color: Colors.white,
-                        ),
+                        hintStyle: TextStyle(color: Colors.white),
                       ),
-                      hintStyle: TextStyle(color: Colors.white),
                     ),
-                  ),
-                  Container(
-                    height: 45,
-                    width: double.infinity,
-                    child: RaisedButton(
-                        child: Text("Zaloguj się"),
-                        color: Colors.white,
-                        onPressed: () {}),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        child: Text('Zarejestruj się'),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/signUp');
-                        },
-                        style: TextButton.styleFrom(
-                          primary: Colors.white,
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: obserText,
+                      validator: (value) {
+                        if (value == "") {
+                          return "Podaj hasło";
+                        } else if (value.length < 8) {
+                          return "Hasło musi być dłuższe niż 8 znaków";
+                        }
+                        return "";
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Hasło:",
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            FocusScope.of(context).unfocus();
+                            // setState(() {
+                            //   obserText = !obserText;
+                            // });
+                          },
+                          child: Icon(
+                            obserText == true
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.white,
+                          ),
                         ),
+                        hintStyle: TextStyle(color: Colors.white),
                       ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+                    ),
+                    Container(
+                      height: 45,
+                      width: double.infinity,
+
+                      child: RaisedButton(
+                          child: Text("Zaloguj się"),
+                          color: Colors.white,
+                          onPressed: () {
+                            validation();
+                            checkLogin(_emailController, _passwordController)
+                                .then(
+                                  (value) => changeToMain(value),
+                            );
+                            //Navigator.pushNamed(context, '/allProductsScreen');
+                          },
+                          ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/signUp');
+                          },
+                          child: Text(
+                            "Zarejestruj się!",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+
+
+
+
+
 }
+
